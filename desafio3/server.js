62f8dbd743458f25892f57f17f5cc95325901300
Gustavo Raeski <gustavoraeski@outlook.com>
2020-06-22 17:12:27 -0300

    Adicionando rotas para cada curso

--- desafio3/server.js
+++ desafio3/server.js
@@ -10,7 +10,8 @@ server.set("view engine", "njk")
 
 nunjucks.configure("views", {
     express:server,
-    autoescape: false
+    autoescape: false,
+    noCache: true
 })
 
 server.get("/", function (req, res) {
@@ -22,10 +23,28 @@ server.get("/courses", function (req, res) {
 server.get("/about", function (req, res) {
     return res.render("about")
 })
+
+
+server.get("/courses/:id", function(req,res){    
+    const id = req.params.id;                           
+
+    const course = information.find(function(course){   
+        return course.id == id                          
+    })
+
+    if(!course){
+        return res.render("not-found")
+    }
+
+    return res.render("course", { item: course })
+
+});
+
 server.use(function(req, res) {
     res.status(404).render("not-found");
   });
 
+
 server.listen(8080, function (){
     console.log("server is running")
-})
+});
