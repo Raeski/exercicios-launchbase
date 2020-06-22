9eabb1a09a945443fe45ace75a564dc9949db03d
Gustavo Raeski <gustavoraeski@outlook.com>
2020-06-22 09:01:01 -0300

    Passando dados do back para o front

---
+++ desafio3/data.js
@@ -0,0 +1,23 @@
+module.exports = [
+    {  
+    id:"starter",
+    description:"O curso Starter da RocketSeat é um curso introdutório as linguagem de programação JS e algumas de suas bibliotecas.",
+    price:"Free",
+    acess:"Acesse o curso",
+    img:"https://skylab.rocketseat.com.br/static/64c237ccff807c054339a62d53b4b402.svg"
+    },
+    {  
+    id:"launchbase",
+    description:"A launchbase é um bootcamp para iniciantes em programação, ensinando como programar em JS. O curso passa por vários módulos contando desde o front-end até o back-end.<br/> Possue 15 módulos os quais passa por vários conteúdos, desde o front-end até o back-end",
+    price:"Pago",
+    acess:"Acesse o curso",
+    img:"https://skylab.rocketseat.com.br/static/0828532024cb46921a6b5e941f8d788d.svg"
+    },
+    {  
+    id:"gostack",
+    description:"A GoStack é um bootcamp focado nas melhores stacks de JavaScript, as quais são ReactJs, React Native e NodeJS. O curso é considerado um dos melhores do mercado.",
+    price:"Pago",
+    acess:"Acesse o curso",
+    img:"https://skylab.rocketseat.com.br/static/83a178a0653dab1d55e2ed7946465975.svg"
+    },
+]
--- desafio3/server.js
+++ desafio3/server.js
@@ -2,20 +2,22 @@ const express = require('express')
 const nunjucks = require('nunjucks')
 
 const server  = express()
+const information = require("./data")
 
 server.use(express.static('public'))
 
 server.set("view engine", "njk")
 
 nunjucks.configure("views", {
-    express:server
+    express:server,
+    autoescape: false
 })
 
 server.get("/", function (req, res) {
     return res.render("index")
 })
 server.get("/courses", function (req, res) {
-    return res.render("courses")
+    return res.render("courses", {items:information})
 })
 server.get("/about", function (req, res) {
     return res.render("about")
